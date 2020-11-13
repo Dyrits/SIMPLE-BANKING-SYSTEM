27073e256c059259fdcc33e17848e6204474f794
Dyrits <dyrits@outlook.com>
2020-11-13 13:17:35 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Simple Banking System
+
+## About this project
+
+### Related course
+JetBrains Academy | Python Developer  
+Simple Banking System 
+
+### Description of the project by JetBrains Academy
+### About
+Everything goes digital these days, and so does money. Today, most people have credit cards, which save us time, energy and nerves. From not having to carry a wallet full of cash to consumer protection, cards make our lives easier in many ways. In this project, you will develop a simple banking system with database.
+
+### Learning outcomes
+In this project, you will find out how the banking system works and learn about SQL. We'll also see how Luhn algorithm can help us avoid mistakes when entering the card number. As an overall result, you'll get new experience in Python.
+
+### Technologies | Tools
+- Python
+- SQLite
+
+### Details | Comments
+This project has been made from scratch following specific instructions.
+
+### Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate (if any).
+
+### Status
+Completed
+
+#### Last update
+13/11/2020
+(README | 13/11/2020)
---
+++ banking.py
@@ -0,0 +1,218 @@
+#! /usr/bin/env python3
+# coding: utf-8
+from random import randrange
+import sqlite3
+
+
+class Card:
+    def __init__(self):
+        self.id = None
+        self.number = self.generate_card_number()
+        self.pin = self.generate_pin()
+        self.balance = 0
+
+    def generate_card_number(self):
+        issuer_identification_number = "400000"
+        account_identifier = self.__generate_random_number_string(9)
+        incomplete_card_number = issuer_identification_number + account_identifier
+        check_sum_number = self.generate_check_sum_number(incomplete_card_number)
+        return incomplete_card_number + check_sum_number
+
+    def generate_pin(self):
+        return self.__generate_random_number_string(4)
+
+    # This method uses Luhn algorithm.
+    @staticmethod
+    def generate_check_sum_number(incomplete_card_number):
+        sum_values = 0
+        for index, value in enumerate(incomplete_card_number):
+            double_value = int(value) * 2 if not index % 2 else int(value)
+            new_value = double_value - 9 if double_value > 9 else double_value
+            sum_values += new_value
+        for value in range(10):
+            if not (sum_values + value) % 10:
+                return str(value)
+
+    @staticmethod
+    def __generate_random_number_string(size):
+        number = randrange(10 ** size)
+        return str(number).zfill(size)
+
+
+class Banking:
+    cards = {}
+    logged_in = False
+    card = None
+
+    @staticmethod
+    def connect():
+        connection = sqlite3.connect('card.s3db')
+        cursor = connection.cursor()
+        return [connection, cursor]
+
+    @staticmethod
+    def commit_and_close(connection):
+        connection.commit()
+        connection.close()
+
+    @classmethod
+    def db_create_table(cls):
+        [connection, cursor] = cls.connect()
+        query = "CREATE TABLE IF NOT EXISTS card ( id INTEGER PRIMARY KEY, number TEXT, pin TEXT, balance INTEGER DEFAULT 0)"
+        cursor.execute(query)
+        cls.commit_and_close(connection)
+
+    @classmethod
+    def db_insert(cls, card):
+        [connection, cursor] = cls.connect()
+        query = "INSERT INTO card (number, pin, balance) VALUES (?, ?, ?)"
+        data = (card.number, card.pin, card.balance)
+        cursor.execute(query, data)
+        card.id = cursor.lastrowid
+        cls.commit_and_close(connection)
+
+    @classmethod
+    def db_delete(cls, card):
+        [connection, cursor] = cls.connect()
+        query = "DELETE FROM card WHERE id = ?"
+        cursor.execute(query, [card.id])
+        cls.commit_and_close(connection)
+
+    @classmethod
+    def db_update_balance(cls, card, amount):
+        [connection, cursor] = cls.connect()
+        query = "UPDATE card SET balance = ? WHERE id = ?"
+        data = (amount, card.id)
+        cursor.execute(query, data)
+        cls.commit_and_close(connection)
+
+    @classmethod
+    def db_select(cls, card_number):
+        [connection, cursor] = cls.connect()
+        query = "SELECT * FROM card WHERE number = ?"
+        cursor.execute(query, [card_number])
+        data = cursor.fetchone()
+        connection.close()
+        if data:
+            card = Card()
+            card.id, card.number, card.pin, card.balance = data
+            cls.cards[card_number] = card
+
+    @classmethod
+    def do_transfer(cls):
+        print("Transfer")
+        card_number = input("Enter your card number: \n")
+        if card_number == cls.card.number:
+            print("You can't transfer money to the same account!")
+            return cls.display_menu()
+        card = cls.cards.get(card_number)
+        if not card:
+            check_sum_number = Card.generate_check_sum_number(card_number[:-1])
+            if check_sum_number != card_number[-1]:
+                print("Probably you made a mistake in the card number. Please try again!")
+                return cls.display_menu()
+            cls.db_select(card_number)
+            card = cls.cards.get(card_number)
+            if not card:
+                print("Such a card does not exist.")
+                return cls.display_menu()
+        amount = int(input("Enter how much money you want to transfer: \n"))
+        if amount > cls.card.balance:
+            print("Not enough money!")
+        else:
+            cls.card.balance -= amount
+            card.balance += amount
+            cls.db_update_balance(cls.card, cls.card.balance)
+            cls.db_update_balance(card, card.balance)
+            print("Success!")
+        cls.display_menu()
+
+    @classmethod
+    def create_account(cls):
+        card = Card()
+        cls.db_insert(card)
+        cls.cards[card.number] = card
+        print("Your card has been created")
+        print("Your card number:")
+        print(card.number)
+        print("Your card PIN:")
+        print(card.pin)
+        cls.display_menu()
+
+    @classmethod
+    def close_account(cls):
+        cls.db_delete(cls.card)
+        del cls.cards[cls.card.number]
+        print("The account has been closed!")
+        cls.display_menu()
+
+    @classmethod
+    def add_to_balance(cls):
+        amount = int(input("Enter income: \n"))
+        cls.card.balance += amount
+        cls.db_update_balance(cls.card, cls.card.balance)
+        print("Income was added!")
+        cls.display_menu()
+
+    @classmethod
+    def login(cls):
+        card_number = input("Enter your card number: \n")
+        cls.card = cls.cards.get(card_number)
+        if not cls.card:
+            cls.db_select(card_number)
+            cls.card = cls.cards.get(card_number)
+        card_pin = input("Enter your PIN: \n")
+        if cls.card and cls.card.pin == card_pin:
+            cls.logged_in = True
+            print("You have successfully logged in!")
+        else:
+            print("Wrong card number or PIN!")
+            cls.card = None
+        cls.display_menu()
+
+    @classmethod
+    def logout(cls):
+        cls.logged_in = False
+        cls.card = None
+        print("You have successfully logged out!")
+        cls.display_menu()
+
+    @classmethod
+    def get_balance(cls):
+        print(f"Balance: {cls.card.balance}")
+        cls.display_menu()
+
+    @classmethod
+    def exit(cls):
+        print("Bye!")
+
+    @classmethod
+    def display_menu(cls):
+        if not cls.logged_in:
+            options = {
+                "1": cls.create_account,
+                "2": cls.login,
+                "0": cls.exit
+            }
+            selection = input("1. Create an account \n2. Log into account \n0. Exit \n")
+        else:
+            options = {
+                "1": cls.get_balance,
+                "2": cls.add_to_balance,
+                "3": cls.do_transfer,
+                "4": cls.close_account,
+                "5": cls.logout,
+                "0": cls.exit
+            }
+            selection = input("1. Balance \n2. Add income \n3. Do transfer \n4. Close account \n5. Log out \n0. Exit \n")
+        print("\n")
+        if options.get(selection):
+            options.get(selection)()
+        else:
+            print("Invalid option!")
+            cls.display_menu()
+
+
+if __name__ == "__main__":
+    Banking.db_create_table()
+    Banking.display_menu()
